67509586edcb3be084bece2ee4775fa00e3558ac
Denali Schlesinger <89662251+dschlesinger@users.noreply.github.com>
2024-11-04 12:50:23 -0500

    Update firebaseconfig.js
    Minor Fix

--- src/firebaseconfig.js
+++ src/firebaseconfig.js
@@ -1,11 +1,11 @@
 const firebaseConfig = { // process broke so hard coding and revoking api key after hackathon
-	apiKey: "AIzaSyCxcfPcfpA4SWm1tNA5n8hLjvRGd9kGob4", // broke so hard code :( process.env.REACT_APP_FIREBASE_API_KEY,
-	authDomain: "openbnb-d875e.firebaseapp.com", // process.env.REACT_APP_AUTH_DOMAIN,
-	projectId: "openbnb-d875e", // process.env.REACT_APP_PROJECT_ID,
-	storageBucket: "openbnb-d875e.firebasestorage.app", // process.env.REACT_APP_STORAGE_BUCKET,
-	messagingSenderId: "1029024267356", // process.env.REACT_APP_MESSAGING_SENDER_ID,
-	appId: "1:1029024267356:web:e4c72376fe71e37679ba99", // process.env.REACT_APP_APP_ID,
-	measurementId: "G-5K6EJN1D72", // process.env.REACT_APP_MEASUREMENT_ID,
+	apiKey: "...", // broke so hard code :( process.env.REACT_APP_FIREBASE_API_KEY,
+	authDomain: "...", // process.env.REACT_APP_AUTH_DOMAIN,
+	projected: "...", // process.env.REACT_APP_PROJECT_ID,
+	storageBucket: "...", // process.env.REACT_APP_STORAGE_BUCKET,
+	messagingSenderId: "...", // process.env.REACT_APP_MESSAGING_SENDER_ID,
+	appId: "...", // process.env.REACT_APP_APP_ID,
+	measurementId: "...", // process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 console.log(process.env)
